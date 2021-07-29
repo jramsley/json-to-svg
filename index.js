d6ce7912fd0726b10147440d5942ef060b1800b4
jramsley <jramsley@ptc.com>
2021-07-29 12:51:42 -0400

    Rotate points 90 degrees clockwise if a right view

--- index.js
+++ index.js
@@ -13,11 +13,33 @@ if (!file) {
   throw new Error('No file provided');
 }
 
+const right = file.includes('Right');
+
 const jsoneFileName = path.parse(file).name + '.svg';
 const json = JSON.parse(fs.readFileSync(file, 'utf-8'));
 
 const svg = new Svg();
 json.bodyData.forEach(a => {
+  if(right) {
+    Object.keys(a.data).forEach(key => {
+      if(Array.isArray(a.data[key]) && a.data[key].length === 3 && typeof a.data[key][0] === 'number') {
+        const x =  a.data[key][1];
+        const y = -a.data[key][0];
+        a.data[key][0] = x;
+        a.data[key][1] = y;
+      }
+
+      if(key === 'controlPoints') {
+        a.data.controlPoints = a.data.controlPoints.map(controlPoint => {
+          const x =  controlPoint[1];
+          const y = -controlPoint[0];
+          const z = controlPoint[2];
+
+          return [x, y, z];
+        });
+      }
+    });
+  }
   const type = a.type;
   switch (type) {
     case 'line':
